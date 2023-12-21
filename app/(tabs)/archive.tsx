f4108adae63bf5ad2e910734e1220c4674b73095
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2023-12-21 21:59:21 +0300

    Add screens

--- app/(tabs)/archive.tsx
+++ app/(tabs)/archive.tsx
@@ -2,10 +2,10 @@ import { StyleSheet } from 'react-native';
 
 import { Text, View } from '../../components/Themed';
 
-export default function HomeScreen() {
+export default function ArchiveScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>HomeScreen</Text>
+      <Text style={styles.title}>ArchiveScreen</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
     </View>
   );
